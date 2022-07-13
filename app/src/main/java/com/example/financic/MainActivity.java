dc6966e566ed2bcbf301d6161f615d128009be91
Usuario <javichua.a@hotmail.com>
2022-07-13 12:02:18 +0200

    feat: changetomyprofile method

--- app/src/main/java/com/example/financic/MainActivity.java
+++ app/src/main/java/com/example/financic/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
     public void changeToLogIn(View view){
         Intent i = new Intent(this,login.class);
         startActivity(i);
+    }
+
+    public void changeToProfile(View view){
+        /*if(mAuth.getCurrentUser() != null){
+            Intent i = new Intent(this, myprofile.class);
+            startActivity(i);
+        }else {
+            Toast.makeText(this, "Debe iniciar sesion primero", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, login.class);
+            startActivity(i);
+        }*/
     }
 }
