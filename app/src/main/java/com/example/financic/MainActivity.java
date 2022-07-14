11b31549967beb5011e47e021782a6e062988b15
Usuario <javichua.a@hotmail.com>
2022-07-14 10:33:42 +0200

    feat: add myprofile-screen

--- app/src/main/java/com/example/financic/MainActivity.java
+++ app/src/main/java/com/example/financic/MainActivity.java
@@ -7,12 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void changeToSignUp(View view){
@@ -26,13 +32,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeToProfile(View view){
-        /*if(mAuth.getCurrentUser() != null){
-            Intent i = new Intent(this, myprofile.class);
+        if(mAuth.getCurrentUser() != null){
+            Intent i = new Intent(this, MyProfile.class);
             startActivity(i);
         }else {
             Toast.makeText(this, "Debe iniciar sesion primero", Toast.LENGTH_SHORT).show();
             Intent i = new Intent(this, login.class);
             startActivity(i);
-        }*/
+        }
     }
 }
